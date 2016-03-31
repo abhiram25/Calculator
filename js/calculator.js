b785ab9d37b5c2dd9771691cd8d6bba249f7bc61
Abhiram Reddy <abhi.reddy1@gmail.com>
2016-03-31 11:15:30 -0400

    refactor with SASS

---
+++ js/calculator.js
@@ -0,0 +1,60 @@
+var value = [];
+
+$(".keys").click(function() {
+
+
+	var number = parseInt($(this).val());
+
+	console.log(number);
+
+	value.push(number);
+
+	console.log(value);
+
+	value.concat("");
+	parseInt(value);
+	console.log(value);
+
+});
+
+$("#add").click(function() {
+	
+
+
+});
+
+
+
+
+
+$("#clear").click(function() {
+
+});
+
+$("#multiply").click(function() {
+
+});
+
+$("#divide").click(function() {
+	console.log("/");
+});
+
+
+
+
+
+$("#subtract").click(function() {
+
+});
+
+$("#percent").click(function() {
+
+});
+
+	$("#equal").click(function() {
+
+
+});
+
+
+
